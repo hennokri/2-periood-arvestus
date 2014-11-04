4d6e3580d1088ba99f2a878bd1243e428762ec75
Kristi Henno <hennokri@gmail.com>
2014-11-04 10:49:17 +0200

    Finished
    finished

---
+++ ylesanne-1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>II perioodi arvestustöö - Ülesanne 1 - GET päring</title>
+  </head>
+
+  <body>
+    <?php
+        $username = "Kristi";
+        $age = 25;
+    ?>
+    <a href="<?php echo "result.php".'?username='.$username . '&age='.$age?>">Teisele lehele</a>
+  </body>
+</html>
